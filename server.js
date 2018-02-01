8d0c4af35d35a395b7ce814f6ed405822aae17bd
John Gully <john@gully.us>
2018-01-31 20:06:12 -0600

    Added CORS capability

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const combinedHandler = require('./handlers/combined');
 const productsHandler = require('./handlers/products');
 const productHandler = require('./handlers/product');
 
 const app = express();
 
+app.use(cors());
 app.use('/graphql', combinedHandler);
 app.use('/products', productsHandler);
 app.use('/product', productHandler);
